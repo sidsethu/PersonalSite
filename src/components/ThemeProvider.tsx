99d33a8172de47d992a83947915a4457a6c6ebe5
sidsethu <sid.sethu@gmail.com>
2025-04-10 15:15:12 +0530

    More Changes

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import type { ThemeProviderProps } from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: { children: React.ReactNode }) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider(props: ThemeProviderProps) {
+  return <NextThemesProvider {...props} />
 } 
